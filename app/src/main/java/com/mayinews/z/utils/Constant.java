c74d1ec2c1fbfb3fd0501cbf1d7690d2c88acc78
GAI <gehang_it@163.com>
2018-01-11 10:48:18 +0800

    放置常量的工具类

---
+++ app/src/main/java/com/mayinews/z/utils/Constant.java
@@ -0,0 +1,16 @@
+package com.mayinews.z.utils;
+
+/**
+ * Created by h on 2018/1/4 0004.
+ * 静态常量类
+ */
+
+public class Constant {
+    public static final String TITLE_SELECTED="title_selected"; ///**已选中频道的json*/
+    public static final String TITLE_UNSELECTED="title_unselected"; //未选频道的json
+    /**频道对应的请求参数*/
+    public static final String CHANNEL_CODE = "channelCode";
+
+    public static final String NEWS_LIST = "http://m.mayinews.com/appapi/newspullup";
+
+}
